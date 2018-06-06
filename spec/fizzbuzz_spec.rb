a4ff74ba0cf503dd02f1747a3cb4af1db4485e91
Jay Khan <jkhan992@gmail.com>
2018-06-06 18:41:04 +0100

    updated tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,20 +1,19 @@
 require "fizzbuzz"
 
 describe "fizbuzz" do
-
-  it "The program return a number" do
-    expect(fizzbuzz(4)).to eq 4
+  it "returns Fizz when a multiple of 3" do
+    expect(fizzbuzz(6)).to eq "Fizz"
   end
 
-  it "return Fizz when a multiple of 3" do
-    expect(fizzbuzz(9)).to eq "Fizz"
-  end
-
-  it "return Buzz when a multiple of 5" do
+  it "returns Buzz when a multiple of 5" do
     expect(fizzbuzz(10)).to eq "Buzz"
   end
 
-  it "return FizzBuzz when a multiple of 3 and 5" do
-    expect(fizzbuzz(15)). to eq  "FizzBuzz"
+  it "returns FizzBuzz when a multiple of 3 and 5" do
+    expect(fizzbuzz(15)).to eq  "FizzBuzz"
+  end
+
+  it "can be passed a number and returns a number in all other instances" do
+    expect(fizzbuzz(1)).to eq 1
   end
 end
